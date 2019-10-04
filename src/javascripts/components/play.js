4a935fa032ad321f3d4a8038c5b18863b5b4234a
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-10-03 22:11:30 -0500

    added functions

--- src/javascripts/components/play.js
+++ src/javascripts/components/play.js
@@ -1,13 +1,51 @@
 import utilities from '../helpers/utilities';
 
+let fun = 50;
+
 const printPlay = () => {
   let domString = '';
   domString += `
   <div class="playContainer">
   <h3 class="playTitle">Play</h3>
+  <progress id="playBar" max="100" value="${fun}"></progress>
+  <button id="goodPlay">Awesome</button>
+  <button id="badPlay">Fun</button>
   </div>
   `;
   utilities.printToDom('play', domString);
 };
 
-export default { printPlay };
+const superFunChoice = () => {
+  document.getElementById('goodPlay').addEventListener('click', () => {
+    const add = fun + 50;
+    if (add > 100) {
+      fun = 100;
+    } else {
+      fun = add;
+    }
+    document.getElementById('playBar').value = fun;
+  });
+};
+
+const slightlyFunChoice = () => {
+  document.getElementById('badPlay').addEventListener('click', () => {
+    fun += 2;
+    document.getElementById('playBar').value = fun;
+  });
+};
+
+const subtractFromPlayBar = () => {
+  document.getElementById('playBar').value = fun;
+  fun -= 1;
+  const playSubtractByTime = () => {
+    setInterval(subtractFromPlayBar, 3000);
+  };
+  playSubtractByTime();
+};
+
+export default {
+  printPlay,
+  subtractFromPlayBar,
+  superFunChoice,
+  slightlyFunChoice,
+};
